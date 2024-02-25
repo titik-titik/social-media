c41af7687c2aa24fa64ed35ec6923398b461fdcc
Azzam Syakir <azzamsykir@gmail.com>
2024-02-25 17:22:18 +0700

    add middleware dummy

---
+++ internal/delivery/http/middleware/middleware.go
@@ -0,0 +1,13 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Middleware Authentication")
+		next.ServeHTTP(w, r)
+	})
+}
