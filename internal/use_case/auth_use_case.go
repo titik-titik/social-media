7dbb899999b5733f7942bc9ffd0218d6b157bcf1
Azzam Syakir <azzamsykir@gmail.com>
2024-03-01 14:29:56 +0700

    feat(controller): add register function

--- internal/use_case/auth_use_case.go
+++ internal/use_case/auth_use_case.go
@@ -1,6 +1,12 @@
 package use_case
 
-import "social-media/internal/repository"
+import (
+	"social-media/internal/repository"
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type AuthUseCase struct {
 	AuthRepository repository.AuthRepository
@@ -10,4 +16,30 @@ func NewAuthUseCase(AuthRepository repository.AuthRepository) *AuthUseCase {
 	return &AuthUseCase{
 		AuthRepository: AuthRepository,
 	}
+}
+
+func (c *AuthUseCase) Register(id string, name, email, password string) error {
+	// Hashing password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	// Generate a UUID if ID is not provided
+	var userID string
+	if id == "" {
+		uuid := uuid.New()
+		userID = uuid.String()
+	} else {
+		userID = id
+	}
+	// Current time
+	currentTime := time.Now()
+
+	// Save the user to the database using the validated data
+	err = c.AuthRepository.Register(userID, name, email, string(hashedPassword), currentTime, currentTime)
+	if err != nil {
+		return err
+	}
+
+	return err
 }
