337a26ed601164ee4d6da2b3d2fd5fea3d3f6723
Azzam Syakir <azzamsykir@gmail.com>
2024-03-01 14:35:53 +0700

    feat(repository): add function register function

--- internal/repository/auth_repository.go
+++ internal/repository/auth_repository.go
@@ -1,6 +1,9 @@
 package repository
 
-import "database/sql"
+import (
+	"database/sql"
+	"time"
+)
 
 type AuthRepository struct {
 	db *sql.DB
@@ -8,4 +11,13 @@ type AuthRepository struct {
 
 func NewAuthRepository(db *sql.DB) *AuthRepository {
 	return &AuthRepository{db: db}
+}
+func (ar *AuthRepository) Register(id, name, email, hashedPassword string, createdAt, updatedAt time.Time) error {
+	createSQL := `
+	    INSERT INTO users (id, name, email, password, created_at, updated_at)
+	    VALUES (?, ?, ?, ?, CONVERT_TZ(?, '+00:00', '+07:00'), CONVERT_TZ(?, '+00:00', '+07:00'))
+	`
+
+	_, err := ar.db.Exec(createSQL, id, name, email, hashedPassword, createdAt.UTC(), updatedAt.UTC())
+	return err
 }
