8daae59463b07aa0475f96145068d25937fba671
Azzam Syakir <azzamsykir@gmail.com>
2024-03-01 21:36:21 +0700

    feat(response): add errorResponse and SuccessResponse

--- internal/model/response/response.go
+++ internal/model/response/response.go
@@ -1,14 +1,35 @@
 package response
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type Response[T any] struct {
 	Message string `json:"message"`
 	Data    T      `json:"data"`
+	Code    int    `json:"code"`
 }
 
-func NewResponse[T any](message string, data T) *Response[T] {
+func NewResponse[T any](message string, data T, code int) *Response[T] {
 	response := &Response[T]{
 		Message: message,
 		Data:    data,
+		Code:    code,
 	}
 	return response
+}
+
+func ErrorResponse(w http.ResponseWriter, message string, code int) {
+	resp := NewResponse(message, nil, code)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(resp.Code)
+	json.NewEncoder(w).Encode(resp)
+}
+
+func SuccessResponse(w http.ResponseWriter, message string, data interface{}, code int) {
+	resp := NewResponse(message, data, code)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(resp.Code)
+	json.NewEncoder(w).Encode(resp)
 }
