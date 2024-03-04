01e17fb5443ffaa901d70e92957d4984a385b09e
Azzam Syakir <azzamsykir@gmail.com>
2024-03-04 22:37:23 +0700

    add register UseCase function

--- internal/use_case/auth_use_case.go
+++ internal/use_case/auth_use_case.go
@@ -1,7 +1,14 @@
 package use_case
 
 import (
+	"social-media/internal/entity"
+	"social-media/internal/model"
+	model_request "social-media/internal/model/request"
 	"social-media/internal/repository"
+	"time"
+
+	"github.com/guregu/null"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthUseCase struct {
@@ -15,4 +22,38 @@ func NewAuthUseCase(
 		AuthRepository: authRepository,
 	}
 	return authUseCase
+}
+func (authUseCase *AuthUseCase) Register(request *model_request.RegisterRequest) *model.Result[*entity.User] {
+	newUser := &entity.User{
+		Username:  request.Username,
+		Email:     request.Email,
+		Password:  request.Password,
+		CreatedAt: null.NewTime(time.Now().UTC(), true),
+		UpdatedAt: null.NewTime(time.Now().UTC(), true),
+	}
+
+	hashedPassword, hashedPasswordErr := bcrypt.GenerateFromPassword([]byte(request.Password.String), bcrypt.DefaultCost)
+	if hashedPasswordErr != nil {
+		return &model.Result[*entity.User]{
+			Code:    500,
+			Message: "AuthUseCase Register is failed, password hashing is failed.",
+			Data:    nil,
+		}
+	}
+	newUser.Password = null.NewString(string(hashedPassword), true)
+
+	createdUser := authUseCase.AuthRepository.Register(newUser)
+	if createdUser == nil {
+		return &model.Result[*entity.User]{
+			Code:    500,
+			Message: "AuthUseCase Register is failed, user is not created.",
+			Data:    nil,
+		}
+	}
+
+	return &model.Result[*entity.User]{
+		Code:    200,
+		Message: "AuthUseCase Register is succeed.",
+		Data:    createdUser,
+	}
 }
