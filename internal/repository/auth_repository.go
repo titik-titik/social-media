d5afcf72a2f3b6e0cde770bee522ffdfd770db31
Azzam Syakir <azzamsykir@gmail.com>
2024-03-04 22:37:00 +0700

    feat: add register repository function

--- internal/repository/auth_repository.go
+++ internal/repository/auth_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"social-media/internal/config"
+	"social-media/internal/entity"
 )
 
 type AuthRepository struct {
@@ -13,4 +14,40 @@ func NewAuthRepository(databaseConfig *config.DatabaseConfig) *AuthRepository {
 		DatabaseConfig: databaseConfig,
 	}
 	return authRepository
+}
+func (authRepository *AuthRepository) Register(toRegisterUser *entity.User) *entity.User {
+	begin, beginErr := authRepository.DatabaseConfig.CockroachdbOneDatabase.Connection.Begin()
+	if beginErr != nil {
+		panic(beginErr)
+	}
+
+	rows, queryErr := begin.Query(
+		"INSERT INTO \"user\" (id, name, username, email, password, avatar_url, bio, is_verified, created_at, updated_at, deleted_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING id, name, username, email, password, avatar_url, bio, is_verified, created_at, updated_at, deleted_at;",
+		toRegisterUser.Id,
+		toRegisterUser.Name,
+		toRegisterUser.Username,
+		toRegisterUser.Email,
+		toRegisterUser.Password,
+		toRegisterUser.AvatarUrl,
+		toRegisterUser.Bio,
+		toRegisterUser.IsVerified,
+		toRegisterUser.CreatedAt,
+		toRegisterUser.UpdatedAt,
+		toRegisterUser.DeletedAt,
+	)
+	if queryErr != nil {
+		panic(queryErr)
+	}
+
+	createdUsers := deserializeRows(rows)
+	if len(createdUsers) == 0 {
+		return nil
+	}
+
+	commitErr := begin.Commit()
+	if commitErr != nil {
+		panic(commitErr)
+	}
+
+	return createdUsers[0]
 }
