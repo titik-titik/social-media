5e90d2dbf04e7274022db238c26b574da6917d7c
Azzam Syakir <azzamsykir@gmail.com>
2024-03-05 09:44:28 +0700

    fix:register functionality

--- internal/delivery/http/auth_controller.go
+++ internal/delivery/http/auth_controller.go
@@ -1,37 +1,29 @@
 package http
 
-//import (
-//	"encoding/json"
-//	"fmt"
-//	"net/http"
-//	"social-media/internal/model"
-//	"social-media/internal/model/response"
-//	"social-media/internal/use_case"
-//)
-//
-//type AuthController struct {
-//	AuthUseCase *use_case.AuthUseCase
-//}
-//
-//func NewAuthController(AuthUseCase *use_case.AuthUseCase) *AuthController {
-//	return &AuthController{AuthUseCase: AuthUseCase}
-//}
-//
-//func (c *AuthController) Register(w http.ResponseWriter, r *http.Request) {
-//	var req model.RegisterRequest
-//
-//	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-//		response.NewResponse(w, "Failed to read user data from the request", []string{}, http.StatusBadRequest)
-//		return
-//	}
-//
-//	err := c.AuthUseCase.Register(req.Username, req.Password, req.Email)
-//	if err != nil {
-//		errorMessage := fmt.Sprintf("Failed to create user: %v", err)
-//		response.NewResponse(w, errorMessage, []string{}, http.StatusInternalServerError)
-//		return
-//	}
-//
-//	response.NewResponse(w, "Success", []string{}, http.StatusCreated)
-//
-//}
+import (
+	"encoding/json"
+	"net/http"
+	model_request "social-media/internal/model/request"
+	model_response "social-media/internal/model/response"
+	"social-media/internal/use_case"
+)
+
+type AuthController struct {
+	AuthUseCase *use_case.AuthUseCase
+}
+
+func NewAuthController(authUseCase *use_case.AuthUseCase) *AuthController {
+	AuthController := &AuthController{
+		AuthUseCase: authUseCase,
+	}
+	return AuthController
+}
+func (authController *AuthController) Register(writer http.ResponseWriter, reader *http.Request) {
+	request := &model_request.RegisterRequest{}
+	decodeErr := json.NewDecoder(reader.Body).Decode(request)
+	if decodeErr != nil {
+		panic(decodeErr)
+	}
+	result := authController.AuthUseCase.Register(request)
+	model_response.NewResponse(writer, result.Message, result.Data, http.StatusOK)
+}
--- internal/delivery/http/route/root_route.go
+++ internal/delivery/http/route/root_route.go
@@ -7,17 +7,20 @@ import (
 type RootRoute struct {
 	Router    *mux.Router
 	UserRoute *UserRoute
+	AuthRoute *AuthRoute
 	PostRoute *PostRoute
 }
 
 func NewRootRoute(
 	router *mux.Router,
 	userRoute *UserRoute,
+	authRoute *AuthRoute,
 	postRoute *PostRoute,
 ) *RootRoute {
 	rootRoute := &RootRoute{
 		Router:    router,
 		UserRoute: userRoute,
+		AuthRoute: authRoute,
 		PostRoute: postRoute,
 	}
 	return rootRoute
@@ -25,5 +28,6 @@ func NewRootRoute(
 
 func (rootRoute *RootRoute) Register() {
 	rootRoute.UserRoute.Register()
+	rootRoute.AuthRoute.Register()
 	// rootRoute.PostRoute.Register()
 }
