13e260ba71393b512ab3f0061bf57d092c1007c1
Azzam Syakir <azzamsykir@gmail.com>
2024-03-06 19:16:15 +0700

    feat: initialize redis connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"social-media/container"
+	redis_one "social-media/db/redis-one"
 )
 
 func main() {
@@ -13,13 +15,21 @@ func main() {
 
 	address := fmt.Sprintf(
 		"%s:%s",
-		"0.0.0.0",
+		webContainer.Env.App.Host,
 		webContainer.Env.App.Port,
 	)
+	redisManager, err := redis_one.NewRedisConnection()
+	if err != nil {
+		log.Fatalf("Failed to initialize Redis: %v", err)
+	}
+	defer func() {
+		if err := redisManager.Close(); err != nil {
+			log.Printf("Failed to close Redis connection: %v", err)
+		}
+	}()
 	listenAndServeErr := http.ListenAndServe(address, webContainer.Route.Router)
 	if listenAndServeErr != nil {
 		panic(listenAndServeErr)
 	}
-
 	fmt.Println("Web finished.")
 }
--- db/redis-one/redis.go
+++ db/redis-one/redis.go
@@ -3,77 +3,57 @@ package redis_one
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
-	"strconv"
 	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
 )
 
-var RedisClient *redis.Client // RedisClient is the instance of the Redis client
-var RedisAddr string          // Variable to store the Redis address
-var RedisKey string           // RedisKey is the key for storing user data in cache
-
-// InitializeRedis sets up the connection to Redis.
-func InitializeRedis(envPath string) *redis.Client {
-	if err := godotenv.Load(envPath); err != nil { // Using relative path
-		log.Fatalf("Error loading .env file in caching: %v", err)
-	}
-
-	RedisAddr = os.Getenv("REDIS_ADDR")
-	RedisPassword := os.Getenv("REDIS_PW")
-	RedisDBStr := os.Getenv("REDIS_DB")
-
-	RedisDB, err := strconv.Atoi(RedisDBStr)
-	if err != nil {
-		fmt.Println("Error converting Redis DB to integer:", err)
-		os.Exit(1)
-	}
-
-	options := &redis.Options{
-		Addr:     RedisAddr,
-		Password: RedisPassword,
-		DB:       RedisDB,
-	}
-	RedisClient = redis.NewClient(options)
-
-	// Test connection and authentication to Redis
-	ctx := context.Background()
-	_, err = RedisClient.Ping(ctx).Result()
-
-	if err != nil {
-		fmt.Println("Error connecting to Redis:", err)
-		// Stop the program or handle the error according to your needs
-		return nil
-	}
-
-	fmt.Printf("Connected to Redis at: %s\n", RedisAddr)
-
-	return RedisClient
+type RedisManager struct {
+	Client *redis.Client
 }
 
-// CloseRedis closes the Redis connection.
-func CloseRedis() error {
-	if err := RedisClient.Close(); err != nil {
-		fmt.Println("Error closing Redis client:", err)
-
-		return err
+func NewRedisConnection() (*RedisManager, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, fmt.Errorf("error loading .env file: %w", err)
 	}
 
+	redisHost := os.Getenv("REDIS_ONE_HOST")
+	redisPort := os.Getenv("REDIS_ONE_PORT")
+	redisAddr := fmt.Sprintf("%s:%s", redisHost, redisPort)
+	password := os.Getenv("REDIS_ONE_PASSWORD")
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: password,
+	})
+
+	ctx := context.Background()
+	_, err := client.Ping(ctx).Result()
+	if err != nil {
+		client.Close()
+		return nil, fmt.Errorf("error connecting to Redis: %w", err)
+	}
+
+	fmt.Printf("Connected to Redis at: %s\n", redisAddr)
+
+	return &RedisManager{Client: client}, nil
+}
+
+func (rm *RedisManager) Close() error {
+	if err := rm.Client.Close(); err != nil {
+		return fmt.Errorf("error closing Redis client: %w", err)
+	}
 	return nil
 }
 
-// InsertData stores data in cache with a specific Redis key and expiration time.
-func InsertData(redisKey string, data []byte, expirationTime time.Time) error {
+func (rm *RedisManager) InsertData(redisKey string, data []byte, expirationTime time.Time) error {
 	ctx := context.Background()
 	expirationDuration := time.Until(expirationTime)
-
-	err := RedisClient.SetEx(ctx, redisKey, data, expirationDuration).Err()
+	err := rm.Client.SetEx(ctx, redisKey, data, expirationDuration).Err()
 	if err != nil {
 		return fmt.Errorf("error setting data to cache: %w", err)
 	}
-
 	return nil
 }
