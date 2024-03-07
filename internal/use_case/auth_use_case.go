3b2871121c8a584606e3d0b94bf0b19717a0ca93
Azzam Syakir <azzamsykir@gmail.com>
2024-03-07 21:13:15 +0700

    fix:login functionality in useCase

--- internal/use_case/auth_use_case.go
+++ internal/use_case/auth_use_case.go
@@ -1,7 +1,8 @@
 package use_case
 
 import (
-	"social-media/db/redis"
+	"fmt"
+	"net/http"
 	"social-media/internal/entity"
 	"social-media/internal/model"
 	model_request "social-media/internal/model/request"
@@ -15,16 +16,13 @@ import (
 
 type AuthUseCase struct {
 	AuthRepository *repository.AuthRepository
-	RedisManager   *redis.RedisManager
 }
 
 func NewAuthUseCase(
 	authRepository *repository.AuthRepository,
-	redisManager *redis.RedisManager,
 ) *AuthUseCase {
 	authUseCase := &AuthUseCase{
 		AuthRepository: authRepository,
-		RedisManager:   redisManager,
 	}
 	return authUseCase
 }
@@ -68,37 +66,34 @@ func (authUseCase *AuthUseCase) Register(request *model_request.RegisterRequest)
 	}
 }
 
-//func (authUsecase *AuthUseCase) Login(request *model_request.LoginRequest) *model.TokenResult {
-//	if request.Email.String == "" || request.Password.String == "" {
-//		return model.NewTokenResult(http.StatusBadRequest, "Email and password must be provided", "")
-//	}
-//
-//	user, err := authUsecase.AuthRepository.Login(request.Email.String)
-//	if err != nil {
-//		return model.NewTokenResult(http.StatusUnauthorized, fmt.Sprintf("User with email %s not found", request.Email.String), "")
-//	}
-//
-//	err = bcrypt.CompareHashAndPassword([]byte(user.Password.String), []byte(request.Password.String))
-//	if err != nil {
-//		return model.NewTokenResult(http.StatusUnauthorized, "Invalid login credentials", "")
-//	}
-//
-//	accToken := fmt.Sprintf("%s:%s", user.Id.String, uuid.New().String())
-//	accExpiration := time.Now().Add(time.Minute * 15)
-//	accRedisKey := "access_token"
-//
-//	err = authUsecase.RedisManager.InsertData(accRedisKey, []byte(accToken), accExpiration)
-//	if err != nil {
-//		return model.NewTokenResult(http.StatusInternalServerError, "Failed to create access token", "")
-//	}
-//
-//	refToken := fmt.Sprintf("%s:%s", user.Id.String, uuid.New().String())
-//	refExpiration := time.Now().Add(time.Hour * 24 * 7)
-//	refRedisKey := "refresh_token"
-//	err = authUsecase.RedisManager.InsertData(refRedisKey, []byte(refToken), refExpiration)
-//	if err != nil {
-//		return model.NewTokenResult(http.StatusInternalServerError, "Failed to create refresh token", "")
-//	}
-//
-//	return model.NewTokenResult(http.StatusOK, "Login successful", accToken)
-//}
+func (ac *AuthUseCase) Login(request *model_request.LoginRequest) *model.TokenResult {
+	if request.Email.String == "" || request.Password.String == "" {
+		return model.NewTokenResult(http.StatusBadRequest, "Email and password must be provided", "")
+	}
+	user, err := ac.AuthRepository.CheckUser(request.Email.String)
+	if err != nil {
+		return model.NewTokenResult(http.StatusUnauthorized, fmt.Sprintf("User with email %s not found", request.Email.String), "")
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password.String), []byte(request.Password.String))
+	if err != nil {
+		return model.NewTokenResult(http.StatusUnauthorized, "Invalid login credentials", "")
+	}
+
+	accToken := fmt.Sprintf("%s:%s", user.Id.String, uuid.New().String())
+	if accToken == "" {
+		errMsg := "Failed to create access token"
+		return model.NewTokenResult(http.StatusInternalServerError, errMsg, "")
+	}
+	accExpiration := time.Now().Add(time.Minute * 15)
+
+	refToken := fmt.Sprintf("%s:%s", user.Id.String, uuid.New().String())
+	if refToken == "" {
+		errMsg := "Failed to create access token"
+		return model.NewTokenResult(http.StatusInternalServerError, errMsg, "")
+	}
+	refExpiration := time.Now().Add(time.Hour * 24 * 7)
+	id := uuid.New().String()
+	loginUser := ac.AuthRepository.Login(id, accToken, accExpiration, refToken, refExpiration)
+
+	return model.NewTokenResult(http.StatusOK, "Login successful", loginUser)
+}
