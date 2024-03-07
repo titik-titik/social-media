2a8d6a5da0619992915f546f2968a1734ecf35d8
Azzam Syakir <azzamsykir@gmail.com>
2024-03-07 17:30:49 +0700

    feat:add tabel session for auth

--- db/cockroachdb/migration/init.sql
+++ db/cockroachdb/migration/init.sql
@@ -27,6 +27,19 @@ create table "post"
     constraint fk_post_user_id_user_id foreign key (user_id) references "user" (id)
 );
 
-
+drop table if exists "session" cascade;
+create table "session"
+(
+    id                       uuid primary key,
+    user_id                  uuid,
+    access_token             text unique,
+    refresh_token            text unique,
+    access_token_expired_at  timestamptz,
+    refresh_token_expired_at timestamptz,
+    created_at               timestamptz,
+    updated_at               timestamptz,
+    deleted_at               timestamptz,
+    constraint fk_session_user_id_user_id foreign key (user_id) references "user" (id)
+);
 select *
 from "user";
