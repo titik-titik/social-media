2bdc3c50f1265b3d2ad07e17cb92ed7ed2f991dc
Azzam Syakir <azzamsykir@gmail.com>
2024-03-12 06:23:29 +0700

    feat: add UserResponse

--- internal/model/response/search_response.go
+++ internal/model/response/search_response.go
@@ -1,1 +1,12 @@
 package response
+
+import "github.com/guregu/null"
+
+type UserResponse struct {
+	ID          null.String `json:"id,omitempty"`
+	UserId      null.String `json:"user_id,omitempty"`
+	ImageUrl    null.String `json:"image_url,omitempty"`
+	Description null.String `json:"description,omitempty"`
+	CreatedAt   null.Time   `json:"created_at,omitempty"`
+	UpdatedAt   null.Time   `json:"updated_at,omitempty"`
+}
