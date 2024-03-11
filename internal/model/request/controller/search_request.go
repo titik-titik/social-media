af29ff0b619d1d8c3efae9f62c7fd88fe22e2f95
Azzam Syakir <azzamsykir@gmail.com>
2024-03-12 06:23:14 +0700

    feat: add GetAllRequest function

--- internal/model/request/controller/search_request.go
+++ internal/model/request/controller/search_request.go
@@ -1,1 +1,7 @@
 package request
+
+type GetAllUserRequest struct {
+	Limit  string `json:"limit" validate:"required"`
+	Offset string `json:"offset" validate:"gte=0"`
+	Order  string `json:"order" validate:"oneof=desc asc"`
+}
