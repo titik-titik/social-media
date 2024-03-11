039284d1937e5d1e9cbfb9b72d030df2de076a8f
Azzam Syakir <azzamsykir@gmail.com>
2024-03-12 06:22:54 +0700

    feat: add Get function

--- internal/delivery/http/search_controller.go
+++ internal/delivery/http/search_controller.go
@@ -1,16 +1,35 @@
 package http
 
 import (
-	"social-media/internal/use_case"
+	"net/http"
+	model_request "social-media/internal/model/request/controller"
+	"social-media/internal/model/response"
+
+	"github.com/gorilla/mux"
 )
 
 type SearchController struct {
-	UserUseCase *use_case.UserUseCase
+	searchUseCase *use_case.
 }
 
 func NewSearchController(sc *use_case.searchUseCase) *SearchController {
 	SearchController := &SearchController{
-		UserUseCase: sc,
+		searchUseCase: sc,
 	}
 	return SearchController
+}
+func (SearchController *SearchController) Get(writer http.ResponseWriter, reader *http.Request) {
+	limitUser := mux.Vars(reader)["limit"]
+	offsetUser := mux.Vars(reader)["offset"]
+	orderUser := mux.Vars(reader)["order"]
+
+	request := model_request.GetAllUserRequest{
+		Limit:  limitUser,
+		Offset: offsetUser,
+		Order:  orderUser,
+	}
+	result := SearchController.searchUseCase.find(request)
+	response.NewResponse(writer, result)
+
+	response.NewResponse(writer, result)
 }
